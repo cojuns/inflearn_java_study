617809ff07b8d585ab89193e47037d0b713a871c
cojun <dhkdzh70@gmail.com>
2023-12-18 23:33:43 +0900

    2day commit

---
+++ src/WhileEx.java
@@ -0,0 +1,29 @@
+
+
+
+public class WhileEx {
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count <= 10){
+
+            System.out.println(count);
+
+            count ++;
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/WhileEx3.java
@@ -0,0 +1,27 @@
+
+
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+
+        int max = 100;
+        int sum = 0;
+        int count = 1;
+
+        while (count <= max){
+
+            sum += count;
+            count++;
+        }
+        System.out.println(sum);
+
+    }
+}
+
+
+
+
+
+
+
+
